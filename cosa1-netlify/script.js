f5de7e25cbe193bda69f413fda53f53a8043cae4
batmanwhisky <batmanwhisky@outlook.com>
2025-05-27 12:39:08 -0500

    updated the three docs to change function, look

--- cosa1-netlify/script.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('CoSA1 app loaded!');
-  });
---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Example interaction: log a message when the page loads
+  console.log('CoSA1 app loaded!');
+});
